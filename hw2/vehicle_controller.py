3096309740cd534ed8454f39b8777b1c414472dd
David Mattia <dmattia@nd.edu>
2016-09-12 01:54:57 -0400

    added decorator for checking minimum altitude

--- hw2/vehicle_controller.py
+++ hw2/vehicle_controller.py
@@ -1,4 +1,5 @@
 from dronekit import VehicleMode, LocationGlobalRelative, connect
+from functools import wraps
 from time import sleep
 
 import dronekit_sitl
@@ -8,6 +9,30 @@ class VehicleController(object):
   """ Wrapper around dronekit vehicle object to make commands
       synchronous.
   """
+  def require_altitude(min_altitude=10):
+    """ Modifies a function to require a min_altitude.
+        If min_altitude is not met, a warning is printed,
+        but no error is thrown.
+    """
+    def decorator(function):
+      @wraps(function)
+      def inner(self, *args, **kwargs):
+        try:
+          altitude = self.vehicle.location.global_relative_frame.alt
+          if altitude >= min_altitude:
+            print "Altitude check cleared, current altitude is "\
+                + str(altitude) + " meters"
+            return function(self, *args, **kwargs)
+          else:
+            print "Could not run " + function.__name__
+            print "The current altitude of " + str(altitude)\
+                + " does not meet the requirement of " + str(min_altitude)
+        except AttributeError:
+          print "Could not find altitude of vehicle"  
+        return lambda x: x
+      return inner
+    return decorator  
+
   def __init__(self, connection_string=None):
     self.sitl = None
     if connection_string is None:
@@ -76,6 +101,7 @@ class VehicleController(object):
     """
     return self.get_distance_in_meters_to(location) < max_meters
 
+  @require_altitude(40)
   def fly_to(self, destination, speed=10):
     """ Maintains altitude and flies to a new locationGlobal Point.
 
