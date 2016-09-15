b7ec6515fed2e2250a41ab79f607004551b3017d
David Mattia <dmattia@nd.edu>
2016-09-15 12:36:44 -0400

    Have drone fly the shortest circuit path for deliveries

--- hw2/delivery.py
+++ hw2/delivery.py
@@ -1,12 +1,98 @@
 from argparse import ArgumentParser
 from vehicle_controller import VehicleController
 from dronekit import LocationGlobal
+from itertools import permutations
+from math import sqrt
+import re
 
-parser = ArgumentParser(description="Command vehicles")
-parser.add_argument('--connect', help='Vehicle Connection String Target.')
-args = parser.parse_args()
+class DeliverySpot(object):
+  """ A location to deliver an item to.
+      Represents one line from the input text files.
 
-with VehicleController(args.connect) as vehicle_controller:
-  vehicle_controller.arm()
-  vehicle_controller.takeoff_to(100)
-  vehicle_controller.fly_to(LocationGlobal(41.7, -86.237771, 100), speed=15)
+      Each line should be in the order:
+      Person Name, Location Name, Order type, latitude, longitude
+  """
+  def __init__(self, line):
+    """ Read in a line and store line contents into variables
+    """
+    raw_args = re.compile(", *").split(line.strip())
+    try:
+      self.name = raw_args[0]
+      self.location_name = raw_args[1]
+      self.pizza_type = raw_args[2]
+      self.latitude = float(raw_args[3])
+      self.longitude = float(raw_args[4])
+    except IndexError:
+      print "Could not understand: " + line
+      exit(1)
+    except TypeError:
+      print "Ensure latitude and longitude are represented as floats"
+      exit(1)
+
+  def distance_to_in_meters(self, other):
+    """ Returns the distance to another DeliverySpot in meters.
+    """
+    dlat = other.latitude - self.latitude
+    dlong = other.longitude - self.longitude
+    return sqrt(dlat**2 + dlong**2) * 1.113195e5
+
+  def __str__(self):
+    """ toString method for printing
+    """
+    return self.location_name
+
+def find_path_distance_meters(path):
+  """ Takes in a path, a list of DeliverySpots,
+      And returns the distance in meters to
+      travel along that complete path.
+  """
+  path_length = 0
+  for i in xrange(len(path)-1):
+    path_length += path[i].distance_to_in_meters(path[i+1])
+  return path_length
+
+if __name__ == "__main__":
+  # Parse arguments
+  parser = ArgumentParser(description="Command vehicles")
+  parser.add_argument('--connect', help='Vehicle Connection String Target.')
+  args = parser.parse_args()
+  
+  # Read in lines from file
+  with open("SouthBendDelivery.txt", "r") as pizzaFile:
+    lines = pizzaFile.readlines()
+  
+  # Create DeliverySpot objects for each line
+  delivery_locs = map(DeliverySpot, lines)
+  home = delivery_locs[0]
+  to_deliver_to = delivery_locs[1:]
+
+  # Find all possible paths
+  possible_paths = [[home] + list(path) + [home] for path in permutations(to_deliver_to)]
+
+  # Find the path length for each path
+  path_lengths = map(find_path_distance_meters, possible_paths)
+
+  # Create a list of tuple of type (path_list, path_distance)
+  zipped = zip(possible_paths, path_lengths)
+
+  # Find the tuple with the shortest path length
+  min_tuple = min(zipped, key = lambda x: x[1])
+
+  # Get the path from this tuple
+  shortest_path = min_tuple[0]
+
+  # Print shortest path
+  print "Flight location order:"
+  print "\n".join(str(spot) for spot in shortest_path)
+    
+  # Execute flight path
+  with VehicleController(args.connect) as vehicle_controller:
+    vehicle_height = 100
+    vehicle_controller.arm()
+    vehicle_controller.takeoff_to(vehicle_height)
+
+    # Fly to each spot in the path
+    for spot in shortest_path:
+      print "Delivering to " + spot.name + " @ " + spot.location_name
+      loc = LocationGlobal(spot.latitude, spot.longitude, vehicle_height)
+      vehicle_controller.fly_to(loc, speed=100)
--- hw2/vehicle_controller.py
+++ hw2/vehicle_controller.py
@@ -1,9 +1,9 @@
 from dronekit import VehicleMode, LocationGlobalRelative, connect
 from functools import wraps
 from time import sleep
+from math import sqrt
 
 import dronekit_sitl
-import math
 
 class VehicleController(object):
   """ Wrapper around dronekit vehicle object to make commands
@@ -35,12 +35,14 @@ class VehicleController(object):
 
   def __init__(self, connection_string=None):
     self.sitl = None
-    if connection_string is None:
-      self.sitl = dronekit_sitl.start_default()
-      self.connection_string = self.sitl.connection_string
+    self.connection_string = connection_string
 
-    print "Connecting to vehicle on: " + connection_string
-    self.vehicle = connect(connection_string, wait_ready=True)
+    if self.connection_string is None:
+      self.sitl = dronekit_sitl.start_default()
+      self.connection_string = self.sitl.connection_string()
+
+    print "Connecting to vehicle on: " + self.connection_string
+    self.vehicle = connect(self.connection_string, wait_ready=True)
 
   def __enter__(self):
     return self
@@ -86,7 +88,7 @@ class VehicleController(object):
     
     while self.vehicle.location.global_relative_frame.alt < targetAltitude * .95:
       print "Altitude: " + str(self.vehicle.location.global_relative_frame.alt)
-      sleep(1)
+      sleep(3)
 
   def get_distance_in_meters_to(self, destination):
     """ Estimates the difference between two points,
@@ -94,14 +96,14 @@ class VehicleController(object):
     """
     dlat = destination.lat - self.location.lat
     dlong = destination.lon - self.location.lon
-    return math.sqrt(dlat**2 + dlong**2) * 1.113195e5
+    return sqrt(dlat**2 + dlong**2) * 1.113195e5
 
   def is_close_to(self, location, max_meters=10):
     """ Returns if the vehicle is within @max_meters of @location
     """
     return self.get_distance_in_meters_to(location) < max_meters
 
-  @require_altitude(40)
+  @require_altitude(10)
   def fly_to(self, destination, speed=10):
     """ Maintains altitude and flies to a new locationGlobal Point.
 
@@ -111,7 +113,8 @@ class VehicleController(object):
     self.vehicle.arispeed = speed
     self.vehicle.simple_goto(destination)
 
-    while self.is_guided:
-      if self.is_close_to(destination):
-        print "Reached target"
-        break
+    while self.is_guided and not self.is_close_to(destination):
+      print "Remaining: " + str(self.get_distance_in_meters_to(destination))
+      sleep(8)
+
+    print "Reached destination"
