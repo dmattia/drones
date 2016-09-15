82ba5ce48dd8ae6cf196aaf6035b88d0ff9f4d9a
dmattia <dmattia@nd.edu>
2016-09-15 12:56:35 -0400

    Added some missing documentation.  Lowered sleep values to increase drone responsiveness to errors

--- hw2/vehicle_controller.py
+++ hw2/vehicle_controller.py
@@ -1,3 +1,10 @@
+"""
+  File Name: vehicle_controller.py
+  Author: David Mattia
+  Python Version: 2.7
+  Description: module containing the VehicleController Class.
+"""
+
 from dronekit import VehicleMode, LocationGlobalRelative, connect
 from functools import wraps
 from time import sleep
@@ -9,6 +16,7 @@ class VehicleController(object):
   """ Wrapper around dronekit vehicle object to make commands
       synchronous.
   """
+
   def require_altitude(min_altitude=10):
     """ Modifies a function to require a min_altitude.
         If min_altitude is not met, a warning is printed,
@@ -34,6 +42,8 @@ class VehicleController(object):
     return decorator  
 
   def __init__(self, connection_string=None):
+    """ Connects to a vehicle
+    """
     self.sitl = None
     self.connection_string = connection_string
 
@@ -45,9 +55,13 @@ class VehicleController(object):
     self.vehicle = connect(self.connection_string, wait_ready=True)
 
   def __enter__(self):
+    """ Called when a VehicleController object is created in a "with" block.
+    """
     return self
 
   def __exit__(self, exc_type, exc_value, traceback):
+    """ Cleanup at the end of a "with" block.
+    """
     print "Cleaning up VehicleController object"
     self.vehicle.close()
     if self.sitl is not None:
@@ -57,19 +71,21 @@ class VehicleController(object):
   @property
   def location(self):
     """ Returns the current position of the vehicle
-        as a LocationGlobal
+        as a LocationGlobal.
     """
     return self.vehicle.location.global_frame
 
   @property
   def is_guided(self):
+    """ Returns if a vehicle is in guided mode.
+    """
     return self.vehicle.mode.name == "GUIDED"
 
   def arm(self):
-    """ Arms the vehicle
+    """ Arms the vehicle.
     """
     while not self.vehicle.is_armable:
-      "Waiting for vehicle to arm"
+      print "Waiting for vehicle to arm"
       sleep(1)
     print "Vehicle is armable"
   
@@ -82,24 +98,24 @@ class VehicleController(object):
     print "Vehicle is armed"
 
   def takeoff_to(self, targetAltitude):
-    """ Arms a vehicle and rises to @targetAltitude above the ground
+    """ Arms a vehicle and rises to @targetAltitude above the ground.
     """
     self.vehicle.simple_takeoff(targetAltitude)
     
     while self.vehicle.location.global_relative_frame.alt < targetAltitude * .95:
       print "Altitude: " + str(self.vehicle.location.global_relative_frame.alt)
-      sleep(3)
+      sleep(1)
 
   def get_distance_in_meters_to(self, destination):
     """ Estimates the difference between two points,
-        does not take into account the curvature of the earth
+        does not take into account the curvature of the earth.
     """
     dlat = destination.lat - self.location.lat
     dlong = destination.lon - self.location.lon
     return sqrt(dlat**2 + dlong**2) * 1.113195e5
 
   def is_close_to(self, location, max_meters=10):
-    """ Returns if the vehicle is within @max_meters of @location
+    """ Returns if the vehicle is within @max_meters of @location.
     """
     return self.get_distance_in_meters_to(location) < max_meters
 
@@ -115,6 +131,6 @@ class VehicleController(object):
 
     while self.is_guided and not self.is_close_to(destination):
       print "Remaining: " + str(self.get_distance_in_meters_to(destination))
-      sleep(8)
+      sleep(1)
 
     print "Reached destination"
