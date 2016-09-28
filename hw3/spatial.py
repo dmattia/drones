9581d626bc556b248ead3e8bb5b22a73da9b441e
David Mattia <dmattia@nd.edu>
2016-09-28 09:06:26 -0400

    Added non-rectangular spatial example

--- hw3/spatial.py
+++ hw3/spatial.py
@@ -15,7 +15,15 @@ def get_distance_meters(aLocation1, aLocation2):
   return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
 def move_between_points(start_point, end_point, distance_to_move):
+  """ Moves a maximum of @distance_to_move from the @start_point in the direction
+      of @end_point. If @distance_to_move is larger than the distance between
+      the two points, then the endpoint is returned.
+
+      Returns: a LocationGlobal
+  """
   total_dist = get_distance_meters(start_point, end_point)
+  if distance_to_move > total_dist:
+    return end_point
   x_change = (distance_to_move / total_dist) * (end_point.lat - start_point.lat)
   y_change = (distance_to_move / total_dist) * (end_point.lon - start_point.lon)
   return LocationGlobal(
@@ -25,7 +33,10 @@ def move_between_points(start_point, end_point, distance_to_move):
 
 class FlyableArea(object):
   """ A collection of LocationGlobal points that defines free space.
-      All area contained between the Convex hull of all points
+      All area contained between the Convex hull of all points is defined as flyable.
+
+      I chose to use any arbitrary set of coordinates and not a rectangle because I will want
+      non-rectangular spatial areas in the next homework.
   """
   def __init__(self, locationGlobals):
     self._location_globals = locationGlobals
@@ -69,6 +80,11 @@ class FlyableArea(object):
     return vertices
 
   def reduce_by_size(self, num_meters):
+    """ Reduces a shape by @num_meters meters. It does this by moving every verticy on the
+        convex hull of this shape @num_meters meters towards the center of this shape.
+
+        Note: This only adds points on the convex hull to the returned value, not any interior points.
+    """
     locations = []
     for verticy in self.hull_vertices:
       # Find a point that is @num_meters towards the center
@@ -78,13 +94,15 @@ class FlyableArea(object):
     return FlyableArea(locations)
       
   def is_in_area(self, locationGlobal):
+    """ Returns if a given locationGlobal is inside of this shape's convex hull.
+    """
     point = np.array((locationGlobal.lat, locationGlobal.lon))
     hull_path = Path(self._points[self.hull.vertices])
     return hull_path.contains_point((locationGlobal.lat, locationGlobal.lon))
 
   def draw_hull(self):
     """ Draws, but does not display, all points along with an outlined
-        convex hull
+        convex hull. A call to plt.show() must be made to show this shape.
     """
     plt.plot(self._points[:,0], self._points[:,1], 'o')
     for simplex in self.hull.simplices:
@@ -96,27 +114,18 @@ if __name__ == "__main__":
   top = 41.519441
   bottom = 41.518968
 
-  upper_left = LocationGlobal(top, left)
-  upper_right = LocationGlobal(top, right)
-  lower_left = LocationGlobal(bottom, left)
-  lower_right = LocationGlobal(bottom, right)
-  #upper_left = LocationGlobal(left, top)
-  #upper_right = LocationGlobal(right, top)
-  #lower_left = LocationGlobal(left, bottom)
-  #lower_right = LocationGlobal(right, bottom)
-  flyable_area = FlyableArea([upper_left, upper_right, lower_left, lower_right])
+  # Create an area from some random points
+  upper_left = LocationGlobal(left, top)
+  upper_right = LocationGlobal(right, top)
+  lower_left = LocationGlobal(left, bottom)
+  lower_right = LocationGlobal(right, bottom)
+  high = LocationGlobal((right + left) / 2, top + (top - bottom))
+  low = LocationGlobal((right - left) / 3 + left, bottom - (top - bottom)) 
+  flyable_area = FlyableArea([upper_left, upper_right, lower_left, lower_right, high, low])
 
-  smaller_area = flyable_area.reduce_by_size(10)
-  flyable_area.draw_hull()
-  smaller_area.draw_hull()
-  print flyable_area.area_in_meters
-  print smaller_area.area_in_meters
-
-  #to_explore = flyable_area
-  #while to_explore.area_in_meters > 5:
-  #  #self.follow_path(to_explore.hull_vertices)
-  #  print "area is: " + str(to_explore.area_in_meters)
-  #  to_explore.draw_hull()
-  #  to_explore = to_explore.reduce_by_size(num_meters=10)
+  to_explore = flyable_area
+  while to_explore.area_in_meters > 50:
+    to_explore.draw_hull()
+    to_explore = to_explore.reduce_by_size(10)
 
   plt.show()
